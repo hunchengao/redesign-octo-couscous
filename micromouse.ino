4bf886083d94403b666cddf76dcf208bdef9db77
黃子軒 <89917750+Shuantaiwan@users.noreply.github.com>
2022-07-09 14:01:52 +0800

    Update micromouse.ino

--- micromouse.ino
+++ micromouse.ino
@@ -1,3 +1,4 @@
+
 //RGB腳位
 #define RGB_PIN  13
 
@@ -8,7 +9,7 @@
 #define IR4  15
 
 //ULN2003AN
-#define ULN2003_PIN 
+#define ULN2003_PIN
 
 //i2c腳位
 #define SCL_PIN A4
@@ -26,68 +27,67 @@
 #define BIN2 14
 #define PWMB 12
 
-void Motor_Controal(int Spend_A int Spend_B)
+void Motor_Controal(int Spend_A,int Spend_B)
 {
-    
-    
+
+  /*
     ---          ---
     | |          | |
     | |          | |
     ---          ---
-    
+
     ---          ---
     | |          | |
     | |          | |
     ---          ---
-    
-    if (  Spend_A > PWMmax||Spend_A < -PWMmax) Spend_A = PWMmax;
-    else if(Spend_A < 0) 
-        {
-        analogWrite(PWMA,-Spend_A);
-        digitalWrite(AIN1,HIGH);
-        digitalWrite(AIN2,LOW);
-        }
-    else if(Spend_A > 0)
-        {
-        analogWrite(PWMA,Spend_A);
-        digitalWrite(AIN1,HIGH);
-        digitalWrite(AIN2,LOW);
-        }
-        
-    if (  Spend_B > PWMmax||Spend_B < PWMmax) Spend_B = -PWMmax;
-    else if(Spend_B < 0)
-        {
-        analogWrite(PWMA,Spend_B);
-        digitalWrite(AIN1,LOW);
-        digitalWrite(AIN2,HIGH);
-        }
-    else if(Spend_B > 0)
-        {
-        analogWrite(PWMA,Spend_B);
-        digitalWrite(AIN1,LOW);
-        digitalWrite(AIN2,HIGH);
-        }
+  */
+  if (  Spend_A > PWMmax || Spend_A < -PWMmax) Spend_A = PWMmax;
+  else if (Spend_A < 0)
+  {
+    analogWrite(PWMA, -Spend_A);
+    digitalWrite(AIN1, HIGH);
+    digitalWrite(AIN2, LOW);
+  }
+  else if (Spend_A > 0)
+  {
+    analogWrite(PWMA, Spend_A);
+    digitalWrite(AIN1, HIGH);
+    digitalWrite(AIN2, LOW);
+  }
+
+  if (  Spend_B > PWMmax || Spend_B < PWMmax) Spend_B = -PWMmax;
+  else if (Spend_B < 0)
+  {
+    analogWrite(PWMA, Spend_B);
+    digitalWrite(AIN1, LOW);
+    digitalWrite(AIN2, HIGH);
+  }
+  else if (Spend_B > 0)
+  {
+    analogWrite(PWMA, Spend_B);
+    digitalWrite(AIN1, LOW);
+    digitalWrite(AIN2, HIGH);
+  }
 }
 
-void setup() {   
-    pinMode(RGB_PIN, OUTPUT);
-    pinMode(IR1, INPUT);
-    pinMode(IR2, INPUT);
-    pinMode(IR3, INPUT);
-    pinMode(IR4, INPUT); 
-    pinMode(PWMA, OUTPUT);
-    pinMode(AIN2, OUTPUT);
-    pinMode(AIN1, OUTPUT);
-    pinMode(STBY, OUTPUT);
-    pinMode(BIN1, OUTPUT);
-    pinMode(BIN2, OUTPUT);
-    pinMode(PWMB, OUTPUT);
+void setup() {
+  pinMode(RGB_PIN, OUTPUT);
+  pinMode(IR1, INPUT);
+  pinMode(IR2, INPUT);
+  pinMode(IR3, INPUT);
+  pinMode(IR4, INPUT);
+  pinMode(PWMA, OUTPUT);
+  pinMode(AIN2, OUTPUT);
+  pinMode(AIN1, OUTPUT);
+  pinMode(STBY, OUTPUT);
+  pinMode(BIN1, OUTPUT);
+  pinMode(BIN2, OUTPUT);
+  pinMode(PWMB, OUTPUT);
 
-  
-  Motor_Controal(255,255);
+
+  Motor_Controal(255, 255);
 }
 
 void loop() {
 
 }
-[]
